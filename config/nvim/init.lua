70112a557619a7a49a1b170a79c18bc110e26b26
Guy Boldon <gb@guyboldon.com>
2023-04-15 20:43:43 +0200

    vim: add easy window navigation

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -331,6 +331,26 @@ vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
 
+-- Guy's kepmaps
+
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+
+vim.keymap.set("t", "<A-h>", [[<C-\><C-N><C-w>h]])
+vim.keymap.set("t", "<A-j>", [[<C-\><C-N><C-w>j]])
+vim.keymap.set("t", "<A-k>", [[<C-\><C-N><C-w>k]])
+vim.keymap.set("t", "<A-l>", [[<C-\><C-N><C-w>l]])
+
+vim.keymap.set("i", "<A-h>", [[<C-\><C-N><C-w>h]])
+vim.keymap.set("i", "<A-j>", [[<C-\><C-N><C-w>j]])
+vim.keymap.set("i", "<A-k>", [[<C-\><C-N><C-w>k]])
+vim.keymap.set("i", "<A-l>", [[<C-\><C-N><C-w>l]])
+
+vim.keymap.set("n", "<A-h>", [[<C-w>h]])
+vim.keymap.set("n", "<A-j>", [[<C-w>j]])
+vim.keymap.set("n", "<A-k>", [[<C-w>k]])
+vim.keymap.set("n", "<A-l>", [[<C-w>l]])
+
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
