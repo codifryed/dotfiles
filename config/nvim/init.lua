c6140075ad3f96777254fc0f522a7b563fe46385
Guy Boldon <gb@guyboldon.com>
2023-04-16 13:27:38 +0200

    increase scrollback buffers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -228,7 +228,8 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.incsearch = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 12
+vim.opt.scrollback = 1000000
 vim.opt.isfname:append("@-@")
 
 vim.opt.colorcolumn = "120"
