29abf725352d0f91a8972875e7b4684207195411
Guy Boldon <gb@guyboldon.com>
2023-04-17 19:55:34 +0200

    nvim: use dracula theme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -114,12 +114,20 @@ require('lazy').setup({
     },
   },
 
+  -- {
+  --   -- Theme inspired by Atom
+  --   'navarasu/onedark.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'Mofiqul/dracula.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 
